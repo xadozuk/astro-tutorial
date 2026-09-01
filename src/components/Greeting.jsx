3bb268ede8f9cb379bf897a8c553e49ca1a494b9
Xadozuk <xadozuk@gmail.com>
2026-09-01 17:40:06 +0200

    feat: finish astro tutorial

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,15 @@
+import { useState } from "preact/hooks";
+
+export default function Greetin({ messages }) {
+  const randomMessage = () =>
+    messages[Math.floor(Math.random() * messages.length)];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>New greeting</button>
+    </div>
+  );
+}
